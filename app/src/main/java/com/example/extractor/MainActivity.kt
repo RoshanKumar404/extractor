df8fc7965bdad19668cd50fdd6287c5d0c86474e
Roshankumar404 <roshankuar951@gmail.com>
2025-12-16 15:56:20 +0530

    completed function  that gets the iamge from gallery

--- app/src/main/java/com/example/extractor/MainActivity.kt
+++ app/src/main/java/com/example/extractor/MainActivity.kt
@@ -2,20 +2,28 @@ package com.example.extractor
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.Display
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.example.extractor.ui.theme.ExtractorTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,19 +43,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
 
     Column(
         modifier = modifier.padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        MyButton()
+//        MyButton()
 
         PickImageFromGallery { uri ->
+            selectedImageUri=uri
             Toast.makeText(context, "Image selected:\n$uri", Toast.LENGTH_SHORT).show()
             // Later → send this URI to ML Kit OCR
         }
+        selectedImageUri?.let{uri->
+            DisplayImage(uri)
+        }
     }
 }
+
+@Composable
+fun DisplayImage(uri: Uri) {
+//    TODO("Not yet implemented")
+    Image(
+        painter = rememberAsyncImagePainter(uri),
+        contentDescription = "Selected Image",
+       modifier = Modifier
+           .fillMaxWidth()
+           .height(250.dp)
+    )
+}
+
 
 @Composable
 fun MyButton() {
